4d47380640296185ab59d8a49d5160a64b6e775d
Sun Dong <truepai415@gmail.com>
2022-02-12 01:54:50 +0800

    change for mintyswap

--- src/constants.ts
+++ src/constants.ts
@@ -19,9 +19,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73'
+export const FACTORY_ADDRESS = '0x5708c62d68e0311a9E91a7172a0Cd627eD6cc5fc'
 
-export const INIT_CODE_HASH = '0x00fb7f630766e6a796048ea87d01acd3068e8ff67d078148a3fa3f4a84f69bd5'
+export const INIT_CODE_HASH = '0xa60da9be3235bd0d008ddbfe8bbf510d1f776c4ffedc820146646b02c1ce257f'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
